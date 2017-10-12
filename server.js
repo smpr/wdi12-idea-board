08a0da2130523f3a2fded5a1060824669d082c67
smpr <gcbrown1116@gmail.com>
2017-10-12 11:40:40 -0400

    Prep for herokku

--- server.js
+++ server.js
@@ -15,11 +15,12 @@ connection.on('connected', () => {
 connection.on('error', (err) => {
   console.log('Mongoose default connection error: ' + err);
 }); 
-
+app.use(express.static(__dirname + '/client/build/'));
 app.use(bodyParser.json());
 app.get('/', (req,res) => {
-  res.send('Hello world!')
-})
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
